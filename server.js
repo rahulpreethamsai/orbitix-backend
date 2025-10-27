822bfa80d4a4f59a3b8f081d475f230bd7889256
rahulpreethamsai <chrahulpreethamsai@gmail.com>
2025-10-27 14:42:17 +0530

    "Server Chnage"

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const dotenv = require('dotenv');
 const cors = require('cors');
-const connectDB = require('./config/dbConnection');
-const errorHandler = require('./middlewares/errorhandler');
+const connectDB = require('./src/config/dbConnection')
+const errorHandler = require('./src/middlewares/errorhandler');
 
 dotenv.config();
 connectDB();
@@ -15,13 +15,13 @@ app.use(cors({
 
 app.use(express.json());
 
-app.use('/api/users', require('./routes/userRoute'));
-app.use('/api/attractions', require('./routes/attractionRoute'));
-app.use('/api/events', require('./routes/eventRoute'));
-app.use('/api/bookings', require('./routes/bookingRoute'));
+app.use('/api/users', require('./src/routes/userRoute'));
+app.use('/api/attractions', require('./src/routes/attractionRoute'));
+app.use('/api/events', require('./src/routes/eventRoute'));
+app.use('/api/bookings', require('./src/routes/bookingRoute'));
 
 app.use(errorHandler);
 
 
 const PORT = process.env.PORT || 5000;
-server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
